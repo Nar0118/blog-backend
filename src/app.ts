8d307fe0d75cdcd83e62d86cbaf3e05b730dc52c
Nar0118 <khachatryannarek42@gmail.com>
2025-03-20 20:30:25 +0400

    fixed cors issues

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,23 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const allowCrossDomain = function allowCrossDomain(req, res, next) {
+    console.log('>>>>>>>>>>');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-DAOMAKER, GOOGAPPUID, X-HACKERS, X-Parse-Master-Key, X-Parse-REST-API-Key, X-Parse-Javascript-Key, X-Parse-Application-Id, X-Parse-Client-Version, X-Parse-Session-Token, X-Requested-With, X-Parse-Revocable-Session, Content-Type, Cache-control, csrf-token, user-agent');
+    res.header('Access-Control-Expose-Headers', 'Content-Disposition');
+    if ('OPTIONS' == req.method) { // intercept OPTIONS method
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  };
+  
+  app.use(
+    allowCrossDomain
+  );
+
 // Use the routes
 app.use(userRoutes);
 app.use(postRoutes);
